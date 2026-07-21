eb9d5cf217221d7b53dc472f69b3624887954664
rrron <33317666+rrron@users.noreply.github.com>
2026-07-21 17:13:14 +0200

    SCRUM-9 Add Robyn onboarding controls (#12)

---
+++ src/demo_app/clients/robyn/__init__.py
@@ -0,0 +1,5 @@
+"""Robyn client workflow helpers."""
+
+from demo_app.clients.robyn.onboarding import RobynOnboardingPlan, build_onboarding_plan
+
+__all__ = ["RobynOnboardingPlan", "build_onboarding_plan"]
